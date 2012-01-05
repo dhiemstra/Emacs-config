e7812495b4c448e777d806b8d917e302e45519d5
Danny Hiemstra <dannyhiemstra@gmail.com>
2012-01-05 14:22:30 +0100

    Corrected syntax error

--- rc/settings.el
+++ rc/settings.el
@@ -15,12 +15,9 @@
 (fset 'yes-or-no-p 'y-or-n-p)       ; replace y-e-s by y
 (global-linum-mode 1)               ; enable line numbers
 
-
 ;; turn off the toolbar, scrollbar, fringe; keep the tab bar
-(when *aquamacs-p*
-  (tabbar-mode 1))
+(tabbar-mode -1)
 (tool-bar-mode -1)
-(fringe-mode -1)
 (scroll-bar-mode -1)
 
 ;; emacs git integration

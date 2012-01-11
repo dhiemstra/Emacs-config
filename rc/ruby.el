e41749a4f08e0ae3d432b4bd351a34693e7240e1
Danny Hiemstra <dannyhiemstra@gmail.com>
2012-01-11 20:13:54 +0100

    Add rabl to ruby mode

--- rc/ruby.el
+++ rc/ruby.el
@@ -26,3 +26,4 @@
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rabl" . ruby-mode))

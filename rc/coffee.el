b169ea21dd4df73a01585ab4f3786563158b7724
Danny Hiemstra <dannyhiemstra@gmail.com>
2012-01-29 11:45:14 +0100

    Added SCSS mode

--- rc/coffee.el
+++ rc/coffee.el
@@ -1,4 +1,3 @@
-(add-to-list 'load-path "~/.emacs.d/vendor/coffee-mode")
 (require 'coffee-mode)
 
 (add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
---
+++ rc/scss.el
@@ -0,0 +1,3 @@
+(require 'scss-mode)
+
+(add-to-list 'auto-mode-alist '("\\.scss\\.erb$" . scss-mode))
